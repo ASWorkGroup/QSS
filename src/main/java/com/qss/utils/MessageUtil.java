5c15b65c59b3fb20955ecaf5d2dcc5958bc5a0de
yuanaiqing <yuan_aiqing@yesdata.net>
2017-12-09 22:50:55 +0800

    修正设置错误
    修正设置错误

--- src/main/java/com/qss/utils/MessageUtil.java
+++ src/main/java/com/qss/utils/MessageUtil.java
@@ -1,6 +1,7 @@
 package com.qss.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class MessageUtil {
 
     @Autowired
+    @Qualifier("messageSource")
     private MessageSource messages;
 
     private String getMessage(String key, String ... messages) {
